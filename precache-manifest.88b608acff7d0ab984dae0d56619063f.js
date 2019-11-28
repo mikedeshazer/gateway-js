f5b2c2bb103c2c3a8b876bec7b0fb8433a27c3f5
noiach <gh@noiach.com>
2019-11-28 21:37:58 +1100

    Updates

---
+++ precache-manifest.88b608acff7d0ab984dae0d56619063f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce4258ad0178d7193202777731d9901",
+    "url": "/rensdk-tutorial/index.html"
+  },
+  {
+    "revision": "6f7db74b2213f0e63f24",
+    "url": "/rensdk-tutorial/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "911c1bd5d0cd9dfeb2b4",
+    "url": "/rensdk-tutorial/static/js/2.8fa84a0d.chunk.js"
+  },
+  {
+    "revision": "6f7db74b2213f0e63f24",
+    "url": "/rensdk-tutorial/static/js/main.336060c7.chunk.js"
+  },
+  {
+    "revision": "7d2e4888cb3b14e60e0d",
+    "url": "/rensdk-tutorial/static/js/runtime~main.5596bc2d.js"
+  }
+]);
